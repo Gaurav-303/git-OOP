2b22e54cb584aaaf509a92159be4e13fdc6ff30b
Gaurav-303 <gaurav134mishra@gmail.com>
2025-08-04 14:57:47 +0530

    second commit

---
+++ java/src/BoardingGateSimulation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class BoardingGateSimulation {
+    public static void main(String[] args) {
+        int[] actual = {101, 103, 102, 104};
+        int[] expected = actual.clone();
+        Arrays.sort(expected);
+        int count = 0;
+        for (int i = 0; i < actual.length; i++) {
+            if (actual[i] != expected[i]) {
+                count++;
+            }
+        }
+        System.out.println(count / 2);
+    }
+}
