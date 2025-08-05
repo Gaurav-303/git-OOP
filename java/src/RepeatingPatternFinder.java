d297f075cb2977e0dd9e0c098e3ff8512c1ee8f9
Gaurav-303 <gaurav134mishra@gmail.com>
2025-08-05 12:24:05 +0530

    5th

---
+++ java/src/RepeatingPatternFinder.java
@@ -0,0 +1,25 @@
+public class RepeatingPatternFinder {
+
+    public static void main(String[] args) {
+        String input = "abcabcabc";
+        String pattern = findRepeatingPattern(input);
+        System.out.println(pattern);
+    }
+
+    public static String findRepeatingPattern(String str) {
+        int len = str.length();
+        for (int i = 1; i <= len / 2; i++) {
+            if (len % i == 0) {
+                String sub = str.substring(0, i);
+                StringBuilder repeated = new StringBuilder();
+                for (int j = 0; j < len / i; j++) {
+                    repeated.append(sub);
+                }
+                if (repeated.toString().equals(str)) {
+                    return sub;
+                }
+            }
+        }
+        return str;
+    }
+}
